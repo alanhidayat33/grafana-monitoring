a8c3a153f6023c588ad8509c8ba7d28439ab7861
alanhidayat33 <alanarbanihidayat92@gmail.com>
2024-12-21 15:07:28 +0700

    release:v1.2.14

--- firewall/firewalld/whitelist.sh
+++ firewall/firewalld/whitelist.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# IP addresses and ports to whitelist
+declare -A IP_PORTS=(
+    ["103.123.62.55"]="9080"
+    ["103.123.63.63"]="9100"
+)
+
 # Check if firewalld is installed
 if ! command -v firewall-cmd &> /dev/null; then
     echo "firewalld is not installed. Installing firewalld..."
     sudo yum install -y firewalld
     sudo systemctl enable firewalld
     sudo systemctl start firewalld
+else
+    # Check if firewalld is active
+    if ! sudo systemctl is-active --quiet firewalld; then
+        echo "firewalld is installed but not active. Activating firewalld..."
+        sudo systemctl enable firewalld
+        sudo systemctl start firewalld
+    fi
 fi
-
-sudo systemctl restart firewalld
-
-# IP addresses and ports to whitelist
-declare -A IP_PORTS=(
-    ["103.123.62.55"]="9080"
-    ["103.123.63.63"]="9100"
-)
 
 # Iterate over the associative array and add firewall rules
 for IP in "${!IP_PORTS[@]}"; do
