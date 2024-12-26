c3ad64f46edb3836ef85d931d27d9cb02f3b8f05
alanhidayat33 <alanarbanihidayat92@gmail.com>
2024-12-26 12:16:13 +0700

    release:v1.2.7

--- node_exporter/node_exporter_textfile/textfile_custom_metrics/total_suspended.sh
+++ node_exporter/node_exporter_textfile/textfile_custom_metrics/total_suspended.sh
@@ -4,7 +4,7 @@
 suspended_account="/usr/local/bin/node_exporter_textfile/textfile/total_suspended_account.prom"
 
 # Caputre the output of the ls command into the variable 'total'
-total=$(ls -ll /var/cpanel/suspended/ | head -n 1 | awk '{print $2}')
+total=$(ls /var/cpanel/suspended/ | wc -l)
 
 # Save the total number of suspended hosting account to a textfile
 echo "suspended_hosting_account $total" > "$suspended_account"
