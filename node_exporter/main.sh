81f5ec5a6be0705e10743f88a8d1692bbd39043d
alanhidayat <42029857+alanhidayat33@users.noreply.github.com>
2025-07-02 16:28:33 +0700

    Update main.sh

--- node_exporter/main.sh
+++ node_exporter/main.sh
@@ -4,9 +4,9 @@
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # Run the Node Exporter script
-"$SCRIPT_DIR/node_exporter.sh"
+#"$SCRIPT_DIR/node_exporter.sh"
 
 # Run the Node Exporter Textfile script
-#"$SCRIPT_DIR/node_exporter_textfile/node_exporter_textfile.sh"
+"$SCRIPT_DIR/node_exporter_textfile/node_exporter_textfile.sh"
 
 exit 0
